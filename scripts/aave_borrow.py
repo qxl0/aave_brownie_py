5b8cafe1b0b903bcc108dc2ffb211fa3dc4344e0
Qiang <qxl0@yahoo.com>
2022-02-21 19:42:14 -0500

    Adding aave_borrow()

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,4 +1,9 @@
-
+from scripts.helpful_scripts import get_account
+from brownie import network, config
+from scripts.get_weth import get_weth
 
 def main():
-  pass
+  account = get_account()
+  erc20_address = config["networks"][network.show_active()]["weth_token"]
+  if network.show_active() in ["mainnet-fork-dev"]:
+    get_weth()
