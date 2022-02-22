a9a474fc4ad9b03cbd4428ece4f6824946227e2e
Qiang <qxl0@yahoo.com>
2022-02-21 20:33:51 -0500

    Added desposit() call

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -19,6 +19,19 @@ def main():
   tx = lending_pool.deposit(erc20_address, amount, account.address, 0, {"from": account})
   tx.wait(1)
   print("Deposited!")
+  # how much to borrow??
+  borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+def get_borrowable_data(lending_pool, account):
+  (total_collateral_eth, total_debt_eth, available_borrow_eth, 
+  current_liguidatation_threshold, ltv, health_factor) = lending_pool.getUserAccountData(account.address)
+  available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+  total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+  total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+  print(f"You have {total_collateral_eth} worth of ETH deposited.")
+  print(f"You have {total_debt_eth} worth of ETH borrowed.")
+  print(f"You can borrow {available_borrow_eth} worth of ETH.")
+  return (float(available_borrow_eth), float(total_debt_eth))
 
 def approve_erc20(amount, spender, erc20_address, account):
   print("Approving ERC20 token....")
