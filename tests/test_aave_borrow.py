b340e2c200e401697bfc4730a6104fc1fe0ebb49
Qiang <qxl0@yahoo.com>
2022-02-22 19:26:13 -0500

    Added a test case

---
+++ tests/test_aave_borrow.py
@@ -0,0 +1,19 @@
+from scripts.aave_borrow import (
+    get_asset_price,
+    get_lending_pool,
+    approve_erc20,
+    get_account,
+)
+from brownie import config, network
+
+
+def test_approve_erc20():
+# Arrange
+    account = get_account()
+    lending_pool = get_lending_pool()
+    amount = 1000000000000000000  # 1
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    # Act
+    tx = approve_erc20(amount, lending_pool.address, erc20_address, account)
+    # Assert
+    assert tx is not True
